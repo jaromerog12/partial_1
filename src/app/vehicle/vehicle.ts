54b979083878a1649f6f630ab44d03027093571a
jaromerog12 <ja.romerog12@uniandes.edu.co>
2023-04-29 02:13:00 -0500

    model vehicle

---
+++ src/app/vehicle/vehicle.ts
@@ -0,0 +1,42 @@
+export class Vehicle {
+  id: number;
+  marca: string;
+  linea: string;
+  referencia: string;
+  modelo: number;
+  kilometraje: number;
+  color: string;
+  imagen: string;
+
+  constructor(){
+    this.id = 0;
+    this.marca = '';
+    this.linea = '';
+    this.referencia = '';
+    this.modelo = 0;
+    this.kilometraje = 0;
+    this.color = '';
+    this.imagen = '';
+  }
+
+  static allAttributesInstance (
+    id: number,
+    marca: string,
+    linea: string,
+    referencia: string,
+    modelo: number,
+    kilometraje: number,
+    color: string,
+    imagen: string
+  ) {
+    let vehicle = new Vehicle();
+    vehicle.id -= id;
+    vehicle.marca = marca;
+    vehicle.linea = linea;
+    vehicle.referencia = referencia;
+    vehicle.modelo = modelo;
+    vehicle.kilometraje = kilometraje;
+    vehicle.color = color;
+    vehicle.imagen = imagen;
+  }
+}
